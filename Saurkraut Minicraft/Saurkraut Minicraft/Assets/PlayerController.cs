fdfaf710112c95e7aa41450b8ec1d047aa5a2aaf
Frank <frankjensen156@gmail.com>
2019-03-26 12:04:16 +0100

    basic movement

---
+++ Saurkraut Minicraft/Saurkraut Minicraft/Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 3.0f;
+    public int facingDirection;
+    private Rigidbody2D rg2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Movement(horizontalMovement, verticalMovement);
+        
+    }
+
+    public void Movement(float horizontal, float vertical)
+    {
+        rg2D.velocity = new Vector2(horizontal * Speed, vertical * Speed);
+
+        
+    }
+
+    public void FlipOnVertival()
+    {
+
+    }
+
+    public void FlipOnHorizontal()
+    {
+
+    }
+}
